fe54bef788ebc843247f5289af3c081403ef8c9a
enwave64 <elliottnicholaswatson@gmail.com>
2019-11-03 11:36:38 -0800

    Adding std::sine and wavetable tutorial code. https://docs.juce.com/master/tutorial_wavetable_synth.html

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -1,19 +1,35 @@
 /*
   ==============================================================================
 
-    This file was auto-generated!
+    MainComponent.cpp
 
   ==============================================================================
 */
 
+//https://docs.juce.com/master/tutorial_wavetable_synth.html
+
 #include "MainComponent.h"
 
+//whether we use sineosc or wavetable implementation
+bool useWaveTable = 1;
+
+// First we define a large number of oscillators to evaluate the CPU load of such a number.
+auto numberOfOscillators = 20; 
 //==============================================================================
 MainComponent::MainComponent()
 {
     // Make sure you set the size of the component after
     // you add any child components.
     setSize (800, 600);
+
+	cpuUsageLabel.setText("CPU Usage", dontSendNotification);
+	cpuUsageText.setJustificationType(Justification::right);
+	addAndMakeVisible(cpuUsageLabel);
+	addAndMakeVisible(cpuUsageText);
+
+	//create the sinusoid wavetable
+	//createWavetable();
+	createWavetableHarmonics();
 
     // Some platforms require permissions to open input channels so request that here
     if (RuntimePermissions::isRequired (RuntimePermissions::recordAudio)
@@ -25,7 +41,9 @@ MainComponent::MainComponent()
     else
     {
         // Specify the number of input and output channels that we want to open
-        setAudioChannels (2, 2);
+        setAudioChannels (0, 2);
+
+		startTimer(50);
     }
 }
 
@@ -33,6 +51,12 @@ MainComponent::~MainComponent()
 {
     // This shuts down the audio device and clears the audio source.
     shutdownAudio();
+}
+
+void MainComponent::timerCallback()
+{
+	auto cpu = deviceManager.getCpuUsage() * 100;
+	cpuUsageText.setText(String(cpu, 6) + " %", dontSendNotification);
 }
 
 //==============================================================================
@@ -43,19 +67,92 @@ void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRat
 
     // You can use this function to initialise any resources you might need,
     // but be careful - it will be called on the audio thread, not the GUI thread.
+		
 
-    // For more details, see the help for AudioProcessor::prepareToPlay()
+	//we initialise the oscillators and set their frequencies to play based on the sample rate as follows	
+	for (auto i = 0; i < numberOfOscillators; ++i)
+	{
+		//We also select a random midi note using the Random class by shifting the lowest possible note by 4 octaves (+ 48) 
+		//and defining a range of 3 octaves (* 36.0) starting from that lowest note.
+		// ergo, lowest note 48 (C3), highest note 84 (C6)
+		auto midiNote = Random::getSystemRandom().nextDouble() * 36.0 + 48.0;
+		//auto midiNote = 48 + i * 6;
+
+		//In order to calculate the frequency of that midi note, we use a simple mathematical formula to retrieve the scalar 
+		//to multiply the frequency of A440 with.
+		//Since we know that the midi note number of A440 is 69, by subtracting the midi note by 69 we get the 
+		//semitone distance from A440 that we can then plug into the following formula: 440 * 2 ^ (d / 12)
+		auto frequency = 440.0 * pow(2.0, (midiNote - 69.0) / 12.0);
+
+		if (useWaveTable) 
+		{
+			//WavetableOsc implementation
+			auto* oscillator = new WavetableOscillator(sineTable);
+
+			//Then, we set the frequency of the oscillator by passing the frequency and sample rate as 
+			//arguments to the setFrequency() function. We also add the oscillator to the array of oscillators.
+			oscillator->setFrequency((float)frequency, sampleRate);
+			tabOscillators.add(oscillator);
+		}
+		else 
+		{
+			//SineOsc implementation
+			//For each oscillator, we instantiate a new SineOscillator object that generates a single sine wave voice.
+			auto* oscillator = new SineOscillator();
+
+			//Then, we set the frequency of the oscillator by passing the frequency and sample rate as 
+			//arguments to the setFrequency() function. We also add the oscillator to the array of oscillators.
+			oscillator->setFrequency((float)frequency, sampleRate);
+			oscillators.add(oscillator);
+		}
+	}
+	//Finally, we define the output level by dividing a quiet gain level by the number of oscillators to prevent clipping 
+	//of the signal by summing such a large number of oscillator samples.
+	level = 0.25f / numberOfOscillators;		
 }
 
-void MainComponent::getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill)
+void MainComponent::getNextAudioBlock(const AudioSourceChannelInfo& bufferToFill)
 {
-    // Your audio-processing code goes here!
+	// Your audio-processing code goes here!
 
-    // For more details, see the help for AudioProcessor::getNextAudioBlock()
+	//First, we retrieve the left and right channel pointers to write to the output buffers.
+	auto* leftBuffer = bufferToFill.buffer->getWritePointer(0, bufferToFill.startSample);
+	auto* rightBuffer = bufferToFill.buffer->getWritePointer(1, bufferToFill.startSample);
 
-    // Right now we are not producing any data, in which case we need to clear the buffer
-    // (to prevent the output of random noise)
-    bufferToFill.clearActiveBufferRegion();
+	bufferToFill.clearActiveBufferRegion();
+
+	for (auto oscillatorIndex = 0; oscillatorIndex < numberOfOscillators; ++oscillatorIndex)
+	{
+		//For each oscillator in the array we retrieve a pointer to the oscillator instance.
+		if (useWaveTable) 
+		{
+			auto* oscillator = tabOscillators.getUnchecked(oscillatorIndex);
+			for (auto sample = 0; sample < bufferToFill.numSamples; ++sample)
+			{
+				//Then for each sample in the audio sample buffer we get the sine wave sample and trim the gain with the level variable.
+				auto levelSample = oscillator->getNextSample() * level;
+
+				//Finally we can add that sample value to the left and right channel samples and sum the signal with the other oscillators.
+				leftBuffer[sample] += levelSample;
+				rightBuffer[sample] += levelSample;
+			}
+		}
+			
+		else 
+		{
+			auto* oscillator = oscillators.getUnchecked(oscillatorIndex);
+
+			for (auto sample = 0; sample < bufferToFill.numSamples; ++sample)
+			{
+				//Then for each sample in the audio sample buffer we get the sine wave sample and trim the gain with the level variable.
+				auto levelSample = oscillator->getNextSample() * level;
+
+				//Finally we can add that sample value to the left and right channel samples and sum the signal with the other oscillators.
+				leftBuffer[sample] += levelSample;
+				rightBuffer[sample] += levelSample;
+			}
+		}
+	}
 }
 
 void MainComponent::releaseResources()
@@ -77,7 +174,134 @@ void MainComponent::paint (Graphics& g)
 
 void MainComponent::resized()
 {
-    // This is called when the MainContentComponent is resized.
-    // If you add any child components, this is where you should
-    // update their positions.
+	cpuUsageLabel.setBounds(10, 10, getWidth() - 20, 20);
+	cpuUsageText.setBounds(10, 10, getWidth() - 20, 20);
 }
+
+
+
+/*
+  ==============================================================================
+
+	WavetableOsc
+
+  ==============================================================================
+*/
+
+void MainComponent::createWavetable()
+{
+
+	//In this function, initialise the AudioSampleBuffer by calling the setSize() method by specifying that we only need one channel 
+	//and the number of samples equal to the table size, in our case a resolution of 128. 
+	//Then retrieve the write pointer for that single channel buffer.
+	sineTable.setSize(1, tableSize + 1);
+	auto* samples = sineTable.getWritePointer(0);
+
+	//Next, calculate the angle delta similarly to the SineOscillator, but this time using the table size and thus dividing the full 2pi cycle by 127.
+	auto angleDelta = MathConstants<double>::twoPi / (double)(tableSize - 1);
+	auto currentAngle = 0.0;
+	for (auto i = 0; i < tableSize; ++i)
+	{
+		//Now for each point in our wavetable, retrieve the sine wave value using the std::sin() function, 
+		//assign the value to the buffer sample and increment the current angle by the delta value.
+		auto sample = std::sin(currentAngle);
+		samples[i] = (float)sample;
+		currentAngle += angleDelta;
+	}
+
+	samples[tableSize] = samples[0];
+}
+
+void MainComponent::createWavetableHarmonics()
+{
+	sineTable.setSize(1, tableSize + 1);
+	sineTable.clear();
+	auto* samples = sineTable.getWritePointer(0);
+	int harmonics[] = { 1, 3, 5, 6, 7, 9, 13, 15 };
+	//int harmonics[] = { 1, 2, 4, 6, 8, 10, 12, 14 };
+	float harmonicWeights[] = { 0.5f, 0.1f, 0.05f, 0.125f, 0.09f, 0.005, 0.002f, 0.001f }; // [1]
+	jassert(numElementsInArray(harmonics) == numElementsInArray(harmonicWeights));
+	for (auto harmonic = 0; harmonic < numElementsInArray(harmonics); ++harmonic)
+	{
+		auto angleDelta = MathConstants<double>::twoPi / (double)(tableSize - 1) * harmonics[harmonic]; // [2]
+		auto currentAngle = 0.0;
+		for (auto i = 0; i < tableSize; ++i)
+		{
+			auto sample = std::sin(currentAngle);
+			samples[i] += (float)sample * harmonicWeights[harmonic];                      // [3]
+			currentAngle += angleDelta;
+		}
+	}
+	samples[tableSize] = samples[0];
+}
+
+void WavetableOscillator::setFrequency(float frequency, float sampleRate)
+{
+	auto tableSizeOverSampleRate = subTableSize / sampleRate;
+	tableDelta = frequency * tableSizeOverSampleRate;
+}
+
+forcedinline float WavetableOscillator::getNextSample() noexcept
+{
+	auto tableSize = wavetable.getNumSamples();
+
+	//First, temporarily store the two indices of the wavetable that surround the sample value that we are trying to retrieve. 
+	
+	auto index0 = (unsigned int)currentIndex;  
+	//***used before  when we were wrapping the higher index***
+	//If the higher index goes beyond the size of the wavetable then we wrap the value to the start of the table.
+	//auto index1 = index0 == (tableSize - 1) ? (unsigned int)0 : index0 + 1;
+	auto index1 = index0 + 1;
+
+	//Next, calculate the interpolation value as a fraction between the two indices 
+	//by subtracting the actual current sample by the truncated lower index. 
+	//This should give us a value between 0 .. 1 that defines the fraction.
+	auto frac = currentIndex - (float)index0;  // [7]
+
+	//Then get a pointer to the AudioSampleBuffer and read the values at the two indices and store these values temporarily.
+	auto* table = wavetable.getReadPointer(0); // [8]
+	auto value0 = table[index0];
+	auto value1 = table[index1];
+
+	//The interpolated sample value can then be retrieved by using the standard interpolation formula and the fraction value calculated previously.
+	auto currentSample = value0 + frac * (value1 - value0); // [9]
+
+	//Finally, increment the angle delta of the table and wrap the value around if the value exceeds the table size.
+	if ((currentIndex += tableDelta) > subTableSize)           // [10]
+		currentIndex -= subTableSize;
+
+	return currentSample;
+}
+
+
+/*
+  ==============================================================================
+
+	SineOsc
+
+  ==============================================================================
+*/
+
+//calculate the angle delta via 2pi * (frequency / samplerate)
+void SineOscillator::setFrequency(float frequency, float sampleRate)
+{
+	auto cyclesPerSample = frequency / sampleRate;
+	angleDelta = cyclesPerSample * MathConstants<float>::twoPi;
+}
+
+//called by getNextAudioBlock() on every sample in the buffer to get sample value from oscillator.
+//Here we calculate sample by using std::sin() by passing in currentAngle and then updating currentAngle
+forcedinline float SineOscillator::getNextSample() noexcept {
+	auto currentSample = std::sin(currentAngle);
+	updateAngle();
+	return currentSample;
+}
+
+//update the angle by incrementing with angle delta; wrap the value when exceeding 2PI
+forcedinline void SineOscillator::updateAngle() noexcept {
+	currentAngle += angleDelta;
+
+	if (currentAngle >= MathConstants<float>::twoPi)
+		currentAngle -= MathConstants<float>::twoPi;
+}
+
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -1,7 +1,7 @@
 /*
   ==============================================================================
 
-    This file was auto-generated!
+    MainComponent.h
 
   ==============================================================================
 */
@@ -10,31 +10,91 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 
+
+//https://docs.juce.com/master/tutorial_wavetable_synth.html
+
+//uses std::sin calcullations for 
+class SineOscillator
+{
+	public:
+		SineOscillator() {}
+
+		//calculate the angle delta via 2pi * (frequency / samplerate)
+		void setFrequency(float frequency, float sampleRate);
+
+		//called by getNextAudioBlock() on every sample in the buffer to get sample value from oscillator.
+		//Here we calculate sample by using std::sin() by passing in currentAngle and then updating currentAngle
+		forcedinline float getNextSample() noexcept;
+
+
+		//update the angle by incrementing with angle delta; wrap the value when exceeding 2PI
+		forcedinline void updateAngle() noexcept;
+
+	private:
+		float currentAngle = 0.0f, angleDelta = 0.0f;
+};
+
+
+
+class WavetableOscillator
+{
+	public:
+		WavetableOscillator(const AudioSampleBuffer& wavetableToUse)
+			: wavetable(wavetableToUse),
+			subTableSize (wavetable.getNumSamples() - 1)
+		{
+			jassert(wavetable.getNumChannels() == 1);
+		}
+		
+		//calculate the angle delta via 2pi * (frequency / samplerate)
+		void setFrequency(float frequency, float sampleRate);
+		forcedinline float getNextSample() noexcept;
+
+	private:
+		const AudioSampleBuffer& wavetable;
+		float currentIndex = 0.0f, tableDelta = 0.0f;
+		const int subTableSize;
+};
+
+
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent   : public AudioAppComponent
+class MainComponent   : public AudioAppComponent, public Timer
 {
-public:
-    //==============================================================================
-    MainComponent();
-    ~MainComponent();
+	public:
+		//==============================================================================
+		MainComponent();
+		~MainComponent();
 
-    //==============================================================================
-    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
-    void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override;
-    void releaseResources() override;
+		//==============================================================================
+		void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
+		void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override;
+		void releaseResources() override;
+		void timerCallback() override;
 
-    //==============================================================================
-    void paint (Graphics& g) override;
-    void resized() override;
+		//==============================================================================
+		void paint (Graphics& g) override;
+		void resized() override;
+		void createWavetable();
+		void createWavetableHarmonics();
 
-private:
-    //==============================================================================
-    // Your private member variables go here...
+	private:
+		//==============================================================================
+		//SinOsc std::sin variables
+		float currentAngle = 0.0f, angleDelta = 0.0f;
+		float level = 0.0f;
+		OwnedArray<SineOscillator> oscillators;
+		OwnedArray<WavetableOscillator> tabOscillators;
 
-
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+		//wavetable variables
+		AudioSampleBuffer sineTable;
+		const unsigned int tableSize = 1 << 7; //resolution of 128
+	
+		//CPU monitoring
+		Label cpuUsageLabel;
+		Label cpuUsageText;
+		JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
