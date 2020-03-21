305e740c6be169022f225b12efb48c4f4a17327f
Stathis <47500836+iorhythm@users.noreply.github.com>
2020-03-21 14:17:19 +0000

    - [KarplusStrong branch] .gitignore: excluded Builds and JuceLibraryCode directories - [KarplusStrong branch] MainComponent.h/.cpp: separated class interface and implementation

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -1,34 +1,165 @@
-/*
-  ==============================================================================
-
-    This file was auto-generated!
-
-  ==============================================================================
-*/
 
 #include "MainComponent.h"
 
-StringSynthesiser *strynth;
+StringSynthesiser::StringSynthesiser( double sampleRate, double frequencyInHz )
+{
+    doPluckForNextBuffer.set( false );
+    prepareSynthesiserState( sampleRate, frequencyInHz );
+}
+
+
+void StringSynthesiser::stringPlucked( float pluckPosition )
+{
+    jassert( pluckPosition >= 0.0 && pluckPosition <= 1.0 );
+
+    // we choose a very simple approach to communicate with the audio thread:
+    // simply tell the synth to perform the plucking excitation at the beginning
+    // of the next buffer (= when generateAndAddData is called the next time).
+
+    if( doPluckForNextBuffer.compareAndSetBool( 1, 0 ) )
+    {
+        // plucking in the middle gives the largest amplitude;
+        // plucking at the very ends will do nothing.
+        //amplitude = std::sin(MathConstants<float>::pi * pluckPosition);
+        amplitude = 0.5f;
+        //delayLineLength = (size_t)roundToInt(sampleRateVar / freq * (2 + pluckPosition));
+        prepareSynthesiserState( sampleRateVar, freq * ( 1.0f + pluckPosition ) );
+        //prepareSynthesiserState(sampleRateVar, freq + pluckPosition);
+        exciteInternalBuffer();
+    }
+}
+
+void StringSynthesiser::generateAndAddData( float* outBuffer, int numSamples )
+{
+    if( doPluckForNextBuffer.compareAndSetBool( 0, 1 ) )
+        exciteInternalBuffer();
+
+    // cycle through the delay line and apply a simple averaging filter
+    for( auto i = 0; i < numSamples; ++i )
+    {
+        auto nextPos = ( pos + 1 ) % delayLine.size();
+
+        delayLine[ nextPos ] = ( float ) ( decay * 0.5 * ( delayLine[ nextPos ] + delayLine[ pos ] ) );
+        outBuffer[ i ] += delayLine[ pos ];
+
+        pos = nextPos;
+    }
+}
+
+void StringSynthesiser::prepareSynthesiserState( double sampleRate, double frequencyInHz )
+{
+    delayLineLength = ( size_t ) roundToInt( sampleRate / frequencyInHz );
+    sampleRateVar = sampleRate;
+    //freq = frequencyInHz;
+
+    // we need a minimum delay line length to get a reasonable synthesis.
+    // if you hit this assert, increase sample rate or decrease frequency!
+    jassert( delayLineLength > 50 );
+
+    delayLine.resize( delayLineLength );
+    std::fill( delayLine.begin(), delayLine.end(), 0.0f );
+
+    excitationSample.resize( delayLineLength );
+
+    // as the excitation sample we use random noise between -1 and 1
+    // (as a simple approximation to a plucking excitation)
+
+    std::generate( excitationSample.begin(),
+        excitationSample.end(),
+        [] { return ( Random::getSystemRandom().nextFloat() * 2.0f ) - 1.0f; } );
+}
+
+void StringSynthesiser::exciteInternalBuffer()
+{
+    // fill the buffer with the precomputed excitation sound (scaled with amplitude)
+
+    jassert( delayLine.size() >= excitationSample.size() );
+
+    std::transform( excitationSample.begin(),
+        excitationSample.end(),
+        delayLine.begin(),
+        [ this ]( double sample ) { return static_cast< float > ( amplitude * sample ); } );
+}
 
 //==============================================================================
+StringComponent::StringComponent( int lengthInPixels, Colour stringColour )
+    : length( lengthInPixels ), colour( stringColour )
+{
+    // ignore mouse-clicks so that our parent can get them instead.
+    setInterceptsMouseClicks( false, false );
+    setSize( length, height );
+    startTimerHz( 60 );
+}
+
+//==============================================================================
+void StringComponent::stringPlucked( float pluckPositionRelative )
+{
+    amplitude = maxAmplitude * std::sin( pluckPositionRelative * MathConstants<float>::pi );
+    phase = MathConstants<float>::pi;
+}
+
+void StringComponent::paint( Graphics& g )
+{
+    g.setColour( colour );
+    g.strokePath( generateStringPath(), PathStrokeType( 2.0f ) );
+}
+
+
+Path StringComponent::generateStringPath() const
+{
+    auto y = height / 2.0f;
+
+    Path stringPath;
+    stringPath.startNewSubPath( 0, y );
+    stringPath.quadraticTo( length / 2.0f, y + ( std::sin( phase ) * amplitude ), ( float ) length, y );
+    return stringPath;
+}
+
+//==============================================================================
+void StringComponent::timerCallback()
+{
+    updateAmplitude();
+    updatePhase();
+    repaint();
+}
+
+void StringComponent::updateAmplitude()
+{
+    // this determines the decay of the visible string vibration.
+    amplitude *= 0.99f;
+}
+
+void StringComponent::updatePhase()
+{
+    // this determines the visible vibration frequency.
+    // just an arbitrary number chosen to look OK:
+    auto phaseStep = 400.0f / length;
+
+    phase += phaseStep;
+
+    if( phase >= MathConstants<float>::twoPi )
+        phase -= MathConstants<float>::twoPi;
+}
+
+//==============================================================================
+
 MainComponent::MainComponent()
 {
     // Make sure you set the size of the component after
     // you add any child components.
-	createStringComponents();
-    setSize (800, 600);
+    setSize( 800, 600 );
 
     // Some platforms require permissions to open input channels so request that here
-    if (RuntimePermissions::isRequired (RuntimePermissions::recordAudio)
-        && ! RuntimePermissions::isGranted (RuntimePermissions::recordAudio))
+    if( RuntimePermissions::isRequired( RuntimePermissions::recordAudio )
+        && !RuntimePermissions::isGranted( RuntimePermissions::recordAudio ) )
     {
-        RuntimePermissions::request (RuntimePermissions::recordAudio,
-                                     [&] (bool granted) { if (granted)  setAudioChannels (2, 2); });
+        RuntimePermissions::request( RuntimePermissions::recordAudio,
+            [ & ]( bool granted ) { if( granted )  setAudioChannels( 2, 2 ); } );
     }
     else
     {
         // Specify the number of input and output channels that we want to open
-        setAudioChannels (2, 2);
+        setAudioChannels( 2, 2 );
     }
 }
 
@@ -39,7 +170,7 @@ MainComponent::~MainComponent()
 }
 
 //==============================================================================
-void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
+void MainComponent::prepareToPlay( int samplesPerBlockExpected, double sampleRate )
 {
     // This function will be called when the audio device is started, or when
     // its settings (i.e. sample rate, block size, etc) are changed.
@@ -48,39 +179,17 @@ void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRat
     // but be careful - it will be called on the audio thread, not the GUI thread.
 
     // For more details, see the help for AudioProcessor::prepareToPlay()
-
-	//currentSampleRate = sampleRate;
-	//strynth = new StringSynthesiser(sampleRate, 200.0f);
-	generateStringSynths(sampleRate);
-
 }
 
-void MainComponent::getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill)
+void MainComponent::getNextAudioBlock( const AudioSourceChannelInfo& bufferToFill )
 {
     // Your audio-processing code goes here!
 
     // For more details, see the help for AudioProcessor::getNextAudioBlock()
 
-	bufferToFill.clearActiveBufferRegion();
-
-	for (auto channel = 0; channel < bufferToFill.buffer->getNumChannels(); ++channel)
-	{
-		auto* channelData = bufferToFill.buffer->getWritePointer(channel, bufferToFill.startSample);
-
-		if (channel == 0)
-		{
-
-				//strynth->generateAndAddData(channelData, bufferToFill.numSamples);
-			for (auto synth : stringSynths)
-				synth->generateAndAddData(channelData, bufferToFill.numSamples);
-		}
-		else
-		{
-			memcpy(channelData,
-				bufferToFill.buffer->getReadPointer(0),
-				((size_t)bufferToFill.numSamples) * sizeof(float));
-		}
-	}
+    // Right now we are not producing any data, in which case we need to clear the buffer
+    // (to prevent the output of random noise)
+    bufferToFill.clearActiveBufferRegion();
 }
 
 void MainComponent::releaseResources()
@@ -89,14 +198,13 @@ void MainComponent::releaseResources()
     // restarted due to a setting change.
 
     // For more details, see the help for AudioProcessor::releaseResources()
-	stringSynths.clear();
 }
 
 //==============================================================================
-void MainComponent::paint (Graphics& g)
+void MainComponent::paint( Graphics& g )
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+    g.fillAll( getLookAndFeel().findColour( ResizableWindow::backgroundColourId ) );
 
     // You can add your drawing code here!
 }
@@ -106,17 +214,58 @@ void MainComponent::resized()
     // This is called when the MainContentComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
+}
 
-	auto xPos = 20;
-	auto yPos = 20;
-	auto yDistance = 50;
+void MainComponent::mouseDown( const MouseEvent& e )
+{
+    mouseDrag( e );
+}
 
-	for (auto stringLine : stringLines)
-	{
-		stringLine->setTopLeftPosition(xPos, yPos);
-		yPos += yDistance;
-		addAndMakeVisible(stringLine);
-	}
+void MainComponent::mouseDrag( const MouseEvent& e )
+{
+    for( auto i = 0; i < stringLines.size(); ++i )
+    {
+        auto* stringLine = stringLines.getUnchecked( i );
+
+        if( stringLine->getBounds().contains( e.getPosition() ) )
+        {
+            auto position = ( e.position.x - stringLine->getX() ) / stringLine->getWidth();
+
+            stringLine->stringPlucked( position );
+            stringSynths.getUnchecked( i )->stringPlucked( position );
+        }
+    }
 }
 
 
+MainComponent::StringParameters::StringParameters( int midiNote ) :
+    frequencyInHz( MidiMessage::getMidiNoteInHertz( midiNote ) ),
+    //lengthInPixels((int)(760 / (frequencyInHz / MidiMessage::getMidiNoteInHertz(42))))
+    lengthInPixels( ( int ) ( 760 ) )
+{}
+
+
+Array<MainComponent::StringParameters> MainComponent::getDefaultStringParameters()
+{
+    //return Array<StringParameters>(42, 44, 46, 49, 51, 54, 56, 58, 61, 63, 66, 68, 70);
+    return Array<StringParameters>( 21 );
+}
+
+void MainComponent::createStringComponents()
+{
+    for( auto stringParams : getDefaultStringParameters() )
+    {
+        stringLines.add( new StringComponent( stringParams.lengthInPixels,
+            Colour::fromHSV( Random().nextFloat(), 0.6f, 0.9f, 1.0f ) ) );
+    }
+}
+
+void MainComponent::generateStringSynths( double sampleRate )
+{
+    stringSynths.clear();
+
+    for( auto stringParams : getDefaultStringParameters() )
+    {
+        stringSynths.add( new StringSynthesiser( sampleRate, stringParams.frequencyInHz ) );
+    }
+}
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -1,14 +1,7 @@
-/*
-  ==============================================================================
-
-    This file was auto-generated!
-
-  ==============================================================================
-*/
 
 #pragma once
 
-#include "../JuceLibraryCode/JuceHeader.h"
+#include <JuceHeader.h>
 //==============================================================================
 /**
 	A very basic generator of a simulated plucked string sound, implementing
@@ -26,11 +19,7 @@ public:
 		@param frequencyInHz   The fundamental frequency of the simulated string in
 							   Hertz.
 	*/
-	StringSynthesiser(double sampleRate, double frequencyInHz)
-	{
-		doPluckForNextBuffer.set(false);
-		prepareSynthesiserState(sampleRate, frequencyInHz);
-	}
+	StringSynthesiser( double sampleRate, double frequencyInHz );
 
 	//==============================================================================
 	/** Excite the simulated string by plucking it at a given position.
@@ -38,26 +27,7 @@ public:
 		@param pluckPosition The position of the plucking, relative to the length
 							 of the string. Must be between 0 and 1.
 	*/
-	void stringPlucked(float pluckPosition)
-	{
-		jassert(pluckPosition >= 0.0 && pluckPosition <= 1.0);
-
-		// we choose a very simple approach to communicate with the audio thread:
-		// simply tell the synth to perform the plucking excitation at the beginning
-		// of the next buffer (= when generateAndAddData is called the next time).
-
-		if (doPluckForNextBuffer.compareAndSetBool(1, 0))
-		{
-			// plucking in the middle gives the largest amplitude;
-			// plucking at the very ends will do nothing.
-			//amplitude = std::sin(MathConstants<float>::pi * pluckPosition);
-			amplitude = 0.5f;
-			//delayLineLength = (size_t)roundToInt(sampleRateVar / freq * (2 + pluckPosition));
-			prepareSynthesiserState(sampleRateVar, freq * (1.0f + pluckPosition));
-			//prepareSynthesiserState(sampleRateVar, freq + pluckPosition);
-			exciteInternalBuffer();
-		}
-	}
+	void stringPlucked( float pluckPosition );
 
 	//==============================================================================
 	/** Generate next chunk of mono audio output and add it into a buffer.
@@ -68,59 +38,13 @@ public:
 		@param numSamples Number of samples to generate (make sure that outBuffer
 						  has enough space).
 	*/
-	void generateAndAddData(float* outBuffer, int numSamples)
-	{
-		if (doPluckForNextBuffer.compareAndSetBool(0, 1))
-			exciteInternalBuffer();
-
-		// cycle through the delay line and apply a simple averaging filter
-		for (auto i = 0; i < numSamples; ++i)
-		{
-			auto nextPos = (pos + 1) % delayLine.size();
-
-			delayLine[nextPos] = (float)(decay * 0.5 * (delayLine[nextPos] + delayLine[pos]));
-			outBuffer[i] += delayLine[pos];
-
-			pos = nextPos;
-		}
-	}
+	void generateAndAddData( float* outBuffer, int numSamples );
 
 private:
 	//==============================================================================
-	void prepareSynthesiserState(double sampleRate, double frequencyInHz)
-	{
-		delayLineLength = (size_t)roundToInt(sampleRate / frequencyInHz);
-		sampleRateVar = sampleRate;
-		//freq = frequencyInHz;
+	void prepareSynthesiserState( double sampleRate, double frequencyInHz );
 
-		// we need a minimum delay line length to get a reasonable synthesis.
-		// if you hit this assert, increase sample rate or decrease frequency!
-		jassert(delayLineLength > 50);
-
-		delayLine.resize(delayLineLength);
-		std::fill(delayLine.begin(), delayLine.end(), 0.0f);
-
-		excitationSample.resize(delayLineLength);
-
-		// as the excitation sample we use random noise between -1 and 1
-		// (as a simple approximation to a plucking excitation)
-
-		std::generate(excitationSample.begin(),
-			excitationSample.end(),
-			[] { return (Random::getSystemRandom().nextFloat() * 2.0f) - 1.0f; });
-	}
-
-	void exciteInternalBuffer()
-	{
-		// fill the buffer with the precomputed excitation sound (scaled with amplitude)
-
-		jassert(delayLine.size() >= excitationSample.size());
-
-		std::transform(excitationSample.begin(),
-			excitationSample.end(),
-			delayLine.begin(),
-			[this](double sample) { return static_cast<float> (amplitude * sample); });
-	}
+	void exciteInternalBuffer();
 
 	//==============================================================================
 	const double decay = 0.998;
@@ -134,7 +58,7 @@ private:
 	std::vector<float> excitationSample, delayLine;
 	size_t pos = 0;
 
-	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(StringSynthesiser)
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR( StringSynthesiser )
 };
 
 
@@ -143,68 +67,22 @@ private:
 	This component represents a horizontal vibrating musical string of fixed height
 	and variable length. The string can be excited by calling stringPlucked().
 */
-class StringComponent : public Component,
-	private Timer
+class StringComponent : public Component, private Timer
 {
 public:
-	StringComponent(int lengthInPixels, Colour stringColour)
-		: length(lengthInPixels), colour(stringColour)
-	{
-		// ignore mouse-clicks so that our parent can get them instead.
-		setInterceptsMouseClicks(false, false);
-		setSize(length, height);
-		startTimerHz(60);
-	}
+	StringComponent( int lengthInPixels, Colour stringColour );
 
-	//==============================================================================
-	void stringPlucked(float pluckPositionRelative)
-	{
-		amplitude = maxAmplitude * std::sin(pluckPositionRelative * MathConstants<float>::pi);
-		phase = MathConstants<float>::pi;
-	}
+	void stringPlucked( float pluckPositionRelative );
 
-	void paint(Graphics& g) override
-	{
-		g.setColour(colour);
-		g.strokePath(generateStringPath(), PathStrokeType(2.0f));
-	}
+	void paint( Graphics& g ) override;
 
+	Path generateStringPath() const;
 
-	Path generateStringPath() const
-	{
-		auto y = height / 2.0f;
+	void timerCallback() override;
 
-		Path stringPath;
-		stringPath.startNewSubPath(0, y);
-		stringPath.quadraticTo(length / 2.0f, y + (std::sin(phase) * amplitude), (float)length, y);
-		return stringPath;
-	}
+	void updateAmplitude();
 
-	//==============================================================================
-	void timerCallback() override
-	{
-		updateAmplitude();
-		updatePhase();
-		repaint();
-	}
-
-	void updateAmplitude()
-	{
-		// this determines the decay of the visible string vibration.
-		amplitude *= 0.99f;
-	}
-
-	void updatePhase()
-	{
-		// this determines the visible vibration frequency.
-		// just an arbitrary number chosen to look OK:
-		auto phaseStep = 400.0f / length;
-
-		phase += phaseStep;
-
-		if (phase >= MathConstants<float>::twoPi)
-			phase -= MathConstants<float>::twoPi;
-	}
+	void updatePhase();
 
 private:
 	//==============================================================================
@@ -216,97 +94,57 @@ private:
 	const float maxAmplitude = 12.0f;
 	float phase = 0.0f;
 
-	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(StringComponent)
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR( StringComponent )
 };
 
 //==============================================================================
 /*
-    This component lives inside our window, and this is where you should put all
-    your controls and content.
+	This component lives inside our window, and this is where you should put all
+	your controls and content.
 */
-class MainComponent   : public AudioAppComponent
+class MainComponent : public AudioAppComponent
 {
 public:
-    //==============================================================================
-    MainComponent();
-    ~MainComponent();
+	//==============================================================================
+	MainComponent();
+	~MainComponent();
 
-    //==============================================================================
-    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
-    void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override;
-    void releaseResources() override;
+	//==============================================================================
+	void prepareToPlay( int samplesPerBlockExpected, double sampleRate ) override;
+	void getNextAudioBlock( const AudioSourceChannelInfo& bufferToFill ) override;
+	void releaseResources() override;
 
-    //==============================================================================
-    void paint (Graphics& g) override;
-    void resized() override;
+	//==============================================================================
+	void paint( Graphics& g ) override;
+	void resized() override;
 
 private:
-    //==============================================================================
-    // Your private member variables go here...
+	//==============================================================================
+	// Your private member variables go here...
 	double currentSampleRate = 0.0;
 
-	void mouseDown(const MouseEvent& e) override
-	{
-		mouseDrag(e);
-	}
+	void mouseDown( const MouseEvent& e ) override;
 
-	void mouseDrag(const MouseEvent& e) override
-	{
-		for (auto i = 0; i < stringLines.size(); ++i)
-		{
-			auto* stringLine = stringLines.getUnchecked(i);
-
-			if (stringLine->getBounds().contains(e.getPosition()))
-			{
-				auto position = (e.position.x - stringLine->getX()) / stringLine->getWidth();
-
-				stringLine->stringPlucked(position);
-				stringSynths.getUnchecked(i)->stringPlucked(position);
-			}
-		}
-	}
+	void mouseDrag( const MouseEvent& e ) override;
 
 	//==============================================================================
 	struct StringParameters
 	{
-		StringParameters(int midiNote)
-			: frequencyInHz(MidiMessage::getMidiNoteInHertz(midiNote)),
-			//lengthInPixels((int)(760 / (frequencyInHz / MidiMessage::getMidiNoteInHertz(42))))
-			lengthInPixels((int)(760))
-		{}
+		StringParameters( int midiNote );
 
 		double frequencyInHz;
 		int lengthInPixels;
 	};
 
-	static Array<StringParameters> getDefaultStringParameters()
-	{
-		//return Array<StringParameters>(42, 44, 46, 49, 51, 54, 56, 58, 61, 63, 66, 68, 70);
-		return Array<StringParameters>(21);
-	}
+	static Array<StringParameters> getDefaultStringParameters();
 
-	void createStringComponents()
-	{
-		for (auto stringParams : getDefaultStringParameters())
-		{
-			stringLines.add(new StringComponent(stringParams.lengthInPixels,
-				Colour::fromHSV(Random().nextFloat(), 0.6f, 0.9f, 1.0f)));
-		}
-	}
+	void createStringComponents();
 
-	void generateStringSynths(double sampleRate)
-	{
-		stringSynths.clear();
-
-		for (auto stringParams : getDefaultStringParameters())
-		{
-			stringSynths.add(new StringSynthesiser(sampleRate, stringParams.frequencyInHz));
-		}
-	}
+	void generateStringSynths( double sampleRate );
 
 	//==============================================================================
 	OwnedArray<StringComponent> stringLines;
 	OwnedArray<StringSynthesiser> stringSynths;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR( MainComponent )
 };
